844820812689144ab3c034e0960c63cd3f036ef6
Morgan Bastian <morganbastian07@gmail.com>
2023-05-25 13:49:36 -0400

    created card elements and index.js file

---
+++ index.js
@@ -0,0 +1,11 @@
+/*Make your comments form functional using javascript
+Add an external javascript file
+Create a function that will grab all of the inputs 
+of the form and output the values to console
+Add additional functionality to have the function 
+save the comment to a text file*/
+
+/*Create a theme changer
+Add a theme button that will change the theme of your page
+save the user selected theme to localStorage to remember their setting
+automatically apply the selected theme on page reload*/
